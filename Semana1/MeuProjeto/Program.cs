87682d2ba53dfe2892603c2463614f4a950bef3d
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-11-08 11:10:26 -0300

    Primeira atividade em sala

---
+++ Semana1/MeuProjeto/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("******************");
+Console.WriteLine("******************");
+Console.WriteLine("Hello, World! Paulo Pereira Marques");
+Console.WriteLine("******************");
+Console.WriteLine("******************");
