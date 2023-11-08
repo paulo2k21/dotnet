37890596499adb57fe0a452b362c6808355c9278
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-11-08 12:28:01 -0300

    Aplicando tipos de dados

--- Semana1/MeuProjeto/Program.cs
+++ Semana1/MeuProjeto/Program.cs
@@ -4,3 +4,21 @@ Console.WriteLine("******************");
 Console.WriteLine("Hello, World! Paulo Pereira Marques");
 Console.WriteLine("******************");
 Console.WriteLine("******************"); //fim
+
+#region tipos de dados
+
+int tipoInteiro;
+double tipoDouble;
+bool tipoBool;
+string tipoTexto;
+long tipoInteiroLong;
+
+tipoInteiro = int.MaxValue;
+
+tipoInteiroLong = long.MaxValue;
+
+Console.WriteLine(tipoInteiro);
+
+Console.WriteLine(tipoInteiroLong);
+
+#endregion
