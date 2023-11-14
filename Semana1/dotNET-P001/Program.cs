56f3e3ef81c9705f2563a0a2dce022ca53d9ad9a
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-11-14 20:16:23 -0300

    Resolvendo questões da atividade, separando por #region em um mesmo arquivo .cs

--- Semana1/dotNET-P001/Program.cs
+++ Semana1/dotNET-P001/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Resposta da questão 1
+    // sbyte: Armazena inteiros de 8 bits com sinal (-128 a 127)
+    sbyte mySByte = -50;
+
+    // byte: Armazena inteiros de 8 bits sem sinal (0 a 255)
+    byte myByte = 200;
+
+    // short (ou Int16): Armazena inteiros de 16 bits com sinal (-32,768 a 32,767)
+    short myShort = 1500;
+
+    // ushort (ou UInt16): Armazena inteiros de 16 bits sem sinal (0 a 65,535)
+    ushort myUShort = 60000;
+
+    // int (ou Int32): Armazena inteiros de 32 bits com sinal (-2,147,483,648 a 2,147,483,647)
+    int myInt = -200000;
+
+    // uint (ou UInt32): Armazena inteiros de 32 bits sem sinal (0 a 4,294,967,295)
+    uint myUInt = 4000000000;
+
+    // long (ou Int64): Armazena inteiros de 64 bits com sinal (-9,223,372,036,854,775,808 a 9,223,372,036,854,775,807)
+    long myLong = -9000000000000000000;
+
+    // ulong (ou UInt64): Armazena inteiros de 64 bits sem sinal (0 a 18,446,744,073,709,551,615)
+    ulong myULong = 18000000000000000000;
+
+    // Exibindo os valores
+    Console.WriteLine($"sbyte: {mySByte}");
+    Console.WriteLine($"byte: {myByte}");
+    Console.WriteLine($"short: {myShort}");
+    Console.WriteLine($"ushort: {myUShort}");
+    Console.WriteLine($"int: {myInt}");
+    Console.WriteLine($"uint: {myUInt}");
+    Console.WriteLine($"long: {myLong}");
+    Console.WriteLine($"ulong: {myULong}");
+#endregion
+
+
