539cd87c939fe58b0eaedba3173236d12cc85f5a
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-11-16 16:26:47 -0300

    Revisão completa, criação readme

--- Semana2/dotNeT-P002/Program.cs
+++ Semana2/dotNeT-P002/Program.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿/*Objetivo:
+Desenvolver um sistema de gerenciamento de tarefas que permita aos usuários criar,
+visualizar e gerenciar tarefas.
+Requisitos do Projeto:
+O sistema deve permitir a criação de tarefas, incluindo um título, descrição e data de
+vencimento.
+Deve ser possível listar todas as tarefas criadas.
+Os usuários devem poder marcar tarefas como concluídas.
+O sistema deve fornecer uma lista de tarefas pendentes e uma lista de tarefas
+concluídas.
+Os usuários devem ser capazes de excluir tarefas.
+Implemente uma pesquisa que permita aos usuários encontrar tarefas com base em
+palavras-chave.
+O sistema deve fornecer estatísticas básicas, como o número de tarefas concluídas e
+pendentes, a tarefa mais antiga e a tarefa mais recente.
+Dicas:
+Use um Array ou uma List para armazenar as tarefas.
+Use estruturas de controle de fluxo (if, else, for, while, do-while, foreach) para
+implementar as funcionalidades do sistema.
+Use a classe DateTime para lidar com datas e prazos.
+Solicite entradas do usuário para criar, editar e excluir tarefas.
+Use strings para armazenar os títulos e descrições das tarefas. */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
