e718d9716345f02b6c8d7d6f075c2e3ae2339a24
Paulo Marques <paulomarques.ufsb@gmail.com>
2023-11-22 09:29:52 -0300

    Treinando Lambada

--- Semana3/Program.cs
+++ Semana3/Program.cs
@@ -9,3 +9,29 @@ var tuple3 = (id: 10, nome: "Paulo", born: new DateTime(1986, 06, 14));
 Console.WriteLine($"Tuple 3: {tuple3.id}, {tuple3.nome}, {tuple3.born}");
 
 #endregion
+
+
+#region LambdaExpression
+
+Func<int, int, int> sum = (x, y) => x + y;
+Console.WriteLine($"sum: {sum(10, 20)}");
+
+
+Action<string> greet = name =>
+{
+   string greeting = $"Hello {name}!";
+   Console.WriteLine(greeting);
+};
+string person = Console.ReadLine() ?? "";
+greet(person);
+
+
+Func<string, int, string> isBiggerThan = (string s, int x) => s.Length > x ? "Yes" : "No";
+var size = 5;
+Console.WriteLine($"The text {person} has more than {size} chars? {isBiggerThan(person, size)}");
+
+string[] people = { "Linq", "Nilon", "Favelado" };
+char letter = 'N';
+Console.WriteLine($"People with name started with '{letter}': {string.Join(", ", people.Where(x => x.StartsWith(letter)))}");
+
+#endregion
