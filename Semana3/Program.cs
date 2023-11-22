b29acbf671a164f2f207177488d60feb084494d0
Paulo Marques <paulomarques.ufsb@gmail.com>
2023-11-22 11:38:25 -0300

    Treinando assuntos da terceira aula

--- Semana3/Program.cs
+++ Semana3/Program.cs
@@ -41,3 +41,66 @@ Console.WriteLine($"People with name started with '{letter}': {string.Join(", ",
 Console.WriteLine("Fechou!");
 
 #endregion
+
+#region 
+
+
+    static Tuple<string, int> CriarTuplaNomeIdade(string nome, int idade)
+    {
+        return Tuple.Create(nome, idade);
+    }
+
+  
+       
+        var resultado1 = CriarTuplaNomeIdade("João", 25);
+        var resultado2 = CriarTuplaNomeIdade("Maria", 30);
+        var resultado3 = CriarTuplaNomeIdade("Carlos", 22);
+
+        Console.WriteLine(resultado1);
+        Console.WriteLine(resultado2);
+        Console.WriteLine(resultado3);
+    
+#endregion
+
+
+#region Linq Examples
+
+List<int> list = new() { 1, 2, 3, 4, 5 };
+var squaredList = list.Select(x => x * x);
+Console.WriteLine($"Original List: {string.Join(", ", list)}");
+Console.WriteLine($"Squared  List: {string.Join(", ", squaredList)}");
+// Original List: 1, 2, 3, 4, 5
+// Squared  List: 1, 4, 9, 16, 25
+var summedList = list.Select((x,index) => x + squaredList.ElementAt(index));
+Console.WriteLine($"Summed   List: {string.Join(", ", summedList)}");
+// Summed   List: 2, 6, 12, 20, 30
+
+var listMultipleOfThree = list.Where(x => x % 3 == 0).ToList();
+listMultipleOfThree.AddRange(squaredList.Where(x => x % 3 == 0).ToList());
+listMultipleOfThree.AddRange(summedList.Where(x => x % 3 == 0).ToList());
+Console.WriteLine($"List Multiple of Three: {string.Join(", ", listMultipleOfThree)}");
+// List Multiple of Three: 3, 9, 6, 12, 30
+Console.WriteLine($"List Multiple of Three: {string.Join(", ", listMultipleOfThree.Order())}");
+
+var students = new List<Student>{
+   new Student(1, "Lima", "123456789", new DateTime(1987, 9, 24), new List<string>{"123456789", "73988342986"}),
+   new Student(2, "Luna", "987654321", new DateTime(2019, 1, 12), new List<string>{"123456789", "73987654321"}),
+   new Student(3, "Psquila", "123456789", new DateTime(1984, 11, 24), new List<string>{"123456789", "77988237086"})
+};
+
+var any = students.Any();
+var anyHelder = students.Any(x => x.FullName.Contains("Helder"));
+//var single = students.Single(x => x.Id == 10);
+//throw exception
+var singleOrDefault = students.SingleOrDefault(x => x.Id == 10);
+
+var select = students.Select(x => x.PhoneNumbers);
+var selectMany = students.SelectMany(x => x.PhoneNumbers);
+
+var legalAge = students.Where(x => x.BirthDate <= DateTime.Today.AddYears(-18)).Select(x => x.FullName);
+Console.WriteLine($"Legal age people: {string.Join(", ", legalAge)}");
+
+
+Console.Read();
+#endregion
+
