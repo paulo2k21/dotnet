4091a26910c81497617fb5fe848d6aab12b18d85
Paulo Marques <paulomarques.ufsb@gmail.com>
2023-11-22 09:16:06 -0300

    Treinando Tuples

---
+++ Semana3/Program.cs
@@ -0,0 +1,11 @@
+﻿#region Tuples
+
+
+using System.Data.Common;
+using System.IO.Compression;
+
+var tuple3 = (id: 10, nome: "Paulo", born: new DateTime(1986, 06, 14));
+
+Console.WriteLine($"Tuple 3: {tuple3.id}, {tuple3.nome}, {tuple3.born}");
+
+#endregion
